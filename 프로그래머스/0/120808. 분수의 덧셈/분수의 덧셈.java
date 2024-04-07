23ff8103b4e1ebb67f97df962ee7a79a023a257a
pooreumu <99081225+pooreumu@users.noreply.github.com>
2024-04-07 13:28:25 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -10,7 +10,7 @@ class Solution {
         return result;
     }
     
-    public static int getGCD(int num1, int num2) {
+    private static int getGCD(int num1, int num2) {
         if (num1 % num2 == 0) {
             return num2;
         }
