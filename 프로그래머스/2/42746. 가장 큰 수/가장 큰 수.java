d30e21aa48511cf063d1432d3a4f73ebedc4bc33
pooreumu <99081225+pooreumu@users.noreply.github.com>
2024-04-09 20:14:05 +0900

    [level 2] Title: 가장 큰 수, Time: 4.59 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String[] sNumbers = Arrays
+            .stream(numbers)
+            .sorted()
+            .mapToObj(String::valueOf)
+            .toArray(String[]::new);
+        
+        Arrays.sort(sNumbers, new Comparator<String>() {
+            @Override
+            public int compare(String a, String b) {
+                String order1 = a + b;
+                String order2 = b + a;
+                return order2.compareTo(order1);
+            }
+        });
+        
+        boolean isAllZero = Arrays.stream(sNumbers).allMatch(x -> x.equals("0"));
+        return isAllZero ? "0" : String.join("", sNumbers);
+    }
+}
