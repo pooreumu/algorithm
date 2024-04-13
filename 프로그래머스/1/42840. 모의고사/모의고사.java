078f887bf4d8067fe818891c190b15fb947e3439
pooreumu <99081225+pooreumu@users.noreply.github.com>
2024-04-13 09:24:23 +0900

    [level 1] Title: 모의고사, Time: 2.43 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,47 +1,22 @@
 import java.util.*;
 
 class Solution {
+
 	public int[] solution(int[] answers) {
-		int[] student1Answer = {1, 2, 3, 4, 5};
-		int[] student2Answer = {2, 1, 2, 3, 2, 4, 2, 5};
-		int[] student3Answer = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-		int student1Result = getCorrectCount(answers, student1Answer);
-		int student2Result = getCorrectCount(answers, student2Answer);
-		int student3Result = getCorrectCount(answers, student3Answer);
+		int[] firstStudentAnswer = {1, 2, 3, 4, 5};
+		int[] secondStudentAnswer = {2, 1, 2, 3, 2, 4, 2, 5};
+		int[] thirdStudentAnswer = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+		int firstStudentResult = getCorrectCount(answers, firstStudentAnswer);
+		int secondStudentResult = getCorrectCount(answers, secondStudentAnswer);
+		int thirdStudentResult = getCorrectCount(answers, thirdStudentAnswer);
 
 		List<Map<String, Integer>> result = new ArrayList<>();
 		Map<String, Integer> student1 = new HashMap<>();
 		student1.put("studentNumber", 1);
-		student1.put("correctCount", student1Result);
+		student1.put("correctCount", firstStudentResult);
 		result.add(student1);
-		if (result.get(0).get("correctCount") < student2Result) {
-			Map<String, Integer> student2 = new HashMap<>();
-			student2.put("studentNumber", 2);
-			student2.put("correctCount", student2Result);
-
-			result.clear();
-			result.add(student2);
-		} else if (result.get(0).get("correctCount") == student2Result) {
-			Map<String, Integer> student2 = new HashMap<>();
-			student2.put("studentNumber", 2);
-			student2.put("correctCount", student2Result);
-
-			result.add(student2);
-		} 
-        if (result.get(0).get("correctCount") < student3Result) {
-			Map<String, Integer> student3 = new HashMap<>();
-			student3.put("studentNumber", 3);
-			student3.put("correctCount", student3Result);
-
-			result.clear();
-			result.add(student3);
-		} else if (result.get(0).get("correctCount") == student3Result) {
-			Map<String, Integer> student3 = new HashMap<>();
-			student3.put("studentNumber", 3);
-			student3.put("correctCount", student3Result);
-
-			result.add(student3);
-		}
+		result = updateResult(result, secondStudentResult, 2);
+		result = updateResult(result, thirdStudentResult, 3);
 
 		return result.stream().mapToInt(x -> x.get("studentNumber")).toArray();
 	}
@@ -52,6 +27,22 @@ class Solution {
 			if (answers[i] == studentAnswers[i % studentAnswers.length]) {
 				result++;
 			}
+		}
+		return result;
+	}
+
+	private List<Map<String, Integer>> updateResult(List<Map<String, Integer>> resultInput, int correctCount,
+		int studentNumber) {
+		List<Map<String, Integer>> result = new ArrayList<>(List.copyOf(resultInput));
+		Map<String, Integer> student = new HashMap<>();
+		student.put("studentNumber", studentNumber);
+		student.put("correctCount", correctCount);
+
+		if (result.get(0).get("correctCount") < correctCount) {
+			result.clear();
+			result.add(student);
+		} else if (result.get(0).get("correctCount") == correctCount) {
+			result.add(student);
 		}
 		return result;
 	}
