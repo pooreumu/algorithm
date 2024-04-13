e6cbd960e99abcddc961b04de31850326fde2dd1
pooreumu <99081225+pooreumu@users.noreply.github.com>
2024-04-13 10:56:59 +0900

    [level 2] Title: 프로세스, Time: 1.98 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,27 +2,36 @@ import java.util.*;
 
 class Solution {
 	public int solution(int[] priorities, int location) {
-		LinkedList<Map<String, Integer>> newPriorities = new LinkedList<>();
+		LinkedList<Card> newPriorities = new LinkedList<>();
 		for (int i = 0; i < priorities.length; i++) {
-			Map<String, Integer> map = new HashMap<>();
-			map.put("index", i);
-			map.put("priority", priorities[i]);
-			newPriorities.add(map);
+			newPriorities.add(new Card(i, priorities[i]));
 		}
 
 		int result = 1;
 		while (true) {
-			Map<String, Integer> polled = newPriorities.poll();
-			if (newPriorities.stream()
-				.anyMatch(priority -> priority.get("priority") > Objects.requireNonNull(polled).get("priority"))) {
-				newPriorities.add(polled);
+			Card polledCard = newPriorities.poll();
+			if (
+				newPriorities.stream()
+					.anyMatch(priority -> priority.priority > Objects.requireNonNull(polledCard).priority)
+			) {
+				newPriorities.add(polledCard);
 			} else {
-				if (Objects.requireNonNull(polled).get("index").equals(location)) {
+				if (Objects.requireNonNull(polledCard).index == location) {
 					return result;
 				} else {
 					result++;
 				}
 			}
+		}
+	}
+
+	public class Card {
+		int index;
+		int priority;
+
+		public Card(int index, int priority) {
+			this.index = index;
+			this.priority = priority;
 		}
 	}
 }
