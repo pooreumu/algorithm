8f59a57977e41fba14c90d7124d3971d1c690915
pooreumu <99081225+pooreumu@users.noreply.github.com>
2024-04-14 18:10:41 +0900

    [level 2] Title: H-Index, Time: 0.38 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -4,12 +4,11 @@ class Solution {
     public int solution(int[] citations) {
 		Arrays.sort(citations);
         
-        int result = 0;
         for (int i = 0; i < citations.length; i++) {
             if(citations[i] >= citations.length - i) {
-                result = Math.max(result , citations.length - i);
+                return citations.length - i;
             }
         }
-        return result;
+        return 0;
     }
 }
