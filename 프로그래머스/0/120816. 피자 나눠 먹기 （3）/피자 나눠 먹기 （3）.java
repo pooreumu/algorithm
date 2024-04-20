7d494e7563c0922e4c7367a385ea56cad7291fe7
pooreumu <99081225+pooreumu@users.noreply.github.com>
2024-04-20 09:13:54 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.32 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -1,6 +1,5 @@
 class Solution {
     public int solution(int slice, int n) {
-        float newSlice = slice;
-        return (int) Math.ceil(n / newSlice);
+        return (int) Math.ceil((float) n / slice);
     }
 }
