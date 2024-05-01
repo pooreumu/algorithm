cd5e8ced43c9f5ff8f0ee8e8bb4c9d4c6cebe2ab
pooreumu <99081225+pooreumu@users.noreply.github.com>
2024-05-01 11:13:16 +0900

    [level 0] Title: 가위 바위 보, Time: 2.33 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        for (int i = 0; i < rsp.length(); i++) {
+            if(rsp.charAt(i) == '0'){
+                answer += "5";
+                continue;
+            }
+            if(rsp.charAt(i) == '2'){
+                answer += "0";
+                continue;
+            }
+            if(rsp.charAt(i) == '5'){
+                answer += "2";
+                continue;
+            }
+        }
+        return answer;
+    }
+}
