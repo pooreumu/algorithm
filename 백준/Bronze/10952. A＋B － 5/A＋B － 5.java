70a7c92e75f810964a883e1af4861d2598eb803e
pooreumu <99081225+pooreumu@users.noreply.github.com>
2024-12-07 17:36:47 +0900

    [Bronze V] Title: A+B - 5, Time: 112 ms, Memory: 15928 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	// Exception 처리
+	public static void main(String[] args) throws Exception {
+
+		// BufferedReader 선언
+
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// BufferedWriter 선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		while (true) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			if (a == 0 && b == 0) {
+				break;
+			}
+			bw.write((a + b) + "\n");
+		}
+
+		br.close();
+
+		// 최종 출력
+		bw.flush();
+		bw.close();
+	}
+}
