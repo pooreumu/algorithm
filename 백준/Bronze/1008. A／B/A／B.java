7b5d92a3756274ad83d757d18174c22a937670a7
pooreumu <99081225+pooreumu@users.noreply.github.com>
2024-12-07 16:37:21 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 16108 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	// Exception 처리
+	public static void main(String[] args) throws Exception {
+
+		// BufferedReader 선언
+
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		// BufferedWriter 선언
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+
+
+		bw.write((double)a / b + "\n");
+
+		br.close();
+
+		// 최종 출력
+		bw.flush();
+		bw.close();
+	}
+}
